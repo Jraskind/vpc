300991ac2826c2efd52a67e7503b21b7df803661
atpoverload <atpoverload@gmail.com>
2022-06-16 19:01:40 -0600

    changing the smoke test

--- smoke_test.sh
+++ smoke_test.sh
@@ -1,4 +1,8 @@
 # !/bin/bash
 
-java -cp dacapo.jar:third_party/jrapl/jrapl.jar:java/edu/binghamton/vpc/VpcCallback.class Harness sunflow -c edu.binghamton.vpc.VpcCallback &
-python3 java_multi_probe.py --pid $! --probes='gc__begin,gc__end'
+CURRENT_DIR=$(dirname $0)
+
+java -Dvpc.library.path="${CURRENT_DIR}"/bin -cp lib/dacapo.jar:vpc.jar Harness sunflow -c edu.binghamton.vpc.VpcDacapoCallback
+
+# java -cp lib/dacapo.jar:vpc.jar Harness sunflow -c edu.binghamton.vpc.VpcCallback &
+# python3 java_multi_probe.py --pid $! --probes='gc__begin,gc__end'
