e365e3eba8b6959f3c7ca1d71692c5c58b7e9741
atpoverload <atpoverload@gmail.com>
2022-06-16 19:08:25 -0600

    changing the smoke test slightly

--- smoke_test.sh
+++ smoke_test.sh
@@ -2,8 +2,8 @@
 
 CURRENT_DIR=$(dirname $(realpath $0))
 
-java -Dvpc.library.path="${CURRENT_DIR}"/bin -Dvpc.output.directory="${CURRENT_DIR}"/data \
--cp lib/dacapo.jar:vpc.jar Harness sunflow -c edu.binghamton.vpc.VpcDacapoCallback
+java -Dvpc.library.path="${CURRENT_DIR}"/bin -Dvpc.output.directory="${CURRENT_DIR}" \
+  -cp lib/dacapo.jar:vpc.jar Harness sunflow -c edu.binghamton.vpc.VpcDacapoCallback
 
 # java -cp lib/dacapo.jar:vpc.jar Harness sunflow -c edu.binghamton.vpc.VpcCallback &
 # python3 java_multi_probe.py --pid $! --probes='gc__begin,gc__end'
